b73e5ec0d4565010733fbcf1ae4e1fc8bc42fd32
Rujul Jain <rsj4000@gmail.com>
2020-10-05 15:24:33 -0500

    Created UI, AudioManager, Highscore storing

---
+++ Assets/Scripts/Level01Controller.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Level01Controller : MonoBehaviour
+{
+    [SerializeField] Text _currentScoreTextView;
+    [SerializeField] GameObject _panalToView; 
+
+    int _currentScore;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        //Increase Score
+        //TODO replace with real implementation later
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
+        //Exit Level
+        //TODO bring up popup menu for navigation
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            //_panalToView.SetActive(true);
+            Resume();
+        }
+
+        
+    }
+
+    public void Resume()
+    {
+        bool currentIsActive = _panalToView.activeSelf;
+        _panalToView.SetActive(!currentIsActive);
+    }
+
+    public void setCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ExitLevel()
+    {
+        //compare score to high score
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            //save current score as new high score
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New high score: " + _currentScore);
+        }
+
+        //load new level
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void IncreaseScore(int scoreIncrease)
+    {
+        //increase score
+        _currentScore += scoreIncrease;
+        //update scroe display, so we can see the new score
+        _currentScoreTextView.text =
+            "Score: " + _currentScore.ToString();
+    }
+}
---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    //[SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
+
+    int highScore = 0;
+
+    private void Start()
+    {
+        //load high score display
+        highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+
+        /*if(_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }*/
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text = highScore.ToString();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
