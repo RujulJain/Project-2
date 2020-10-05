6d4a305d971c64e584e926dcbfb8e5b35ff549a9
Rujul Jain <rsj4000@gmail.com>
2020-10-05 15:24:33 -0500

    Created UI, AudioManager, Highscore storing
    02A completed

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip _startingSong;
+
+    public static AudioManager Instance = null;
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        if (_startingSong != null)
+        {
+            AudioManager.Instance.PlaySong(_startingSong);
+        }
+    }
+
+    private void Awake()
+    {
+        #region Singleton Pattern (Simple)
+        if(Instance == null)
+        {
+            //doesn't exist yet, this is now our singleton!
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            //fill references
+            _audioSource = GetComponent<AudioSource>();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        #endregion
+    }
+
+    public void PlaySong(AudioClip clip)
+    {
+        _audioSource.clip = clip;
+        _audioSource.Play();
+    }
+}
